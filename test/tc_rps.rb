bd19692c9ce2d519e8477b9ec6c02f8c0aa1d23a
Diego Williams Aguilar Montaño <alu0100592368@ull.edu.es>
2013-10-29 18:38:25 +0000

    cambiando configuracion de sesion y añadiendo 2 test mas

--- test/tc_rps.rb
+++ test/tc_rps.rb
@@ -7,6 +7,7 @@ class PPTTest < Test::Unit::TestCase
 
   def app
     Rack::Builder.new do
+      use(Rack::Session::Cookie, {:key => 'rack.session', :secret => 'ppt'})
       run RockPaperScissors::App.new
     end.to_app
   end
@@ -40,4 +41,15 @@ class PPTTest < Test::Unit::TestCase
     get "/?choice=Tijera"
     assert last_response.ok?
   end
+
+  def test_h1
+    get "/"
+    assert_match "<h1>SISTEMAS Y TECNOLOGÍAS WEB</h1>", last_response.body
+  end
+
+  def test_h3
+    get "/"
+    assert_match "<h3>Estadísticas del juego para las tiradas del jugador</h3>", last_response.body
+  end
+
 end
