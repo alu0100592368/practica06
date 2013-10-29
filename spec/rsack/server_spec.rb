79f7b2a4ecd2477cb7ba09a64b42c2d5c4100bf6
Diego Williams Aguilar Montaño <alu0100592368@ull.edu.es>
2013-10-29 18:37:42 +0000

    arreglando metodo server para las sesiones y añadiendo 2 test mas

--- spec/rsack/server_spec.rb
+++ spec/rsack/server_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe RockPaperScissors::App do
   #let(:server) { Rack::MockRequest.new(RockPaperScissors::App.new) }
   def server
-    Rack::MockRequest.new(RockPaperScissors::App.new)
+    Rack::MockRequest.new(Rack::Session::Cookie.new(RockPaperScissors::App.new, :key => 'rack.session', :secret => 'ppt'))
   end
 
   context '/' do
@@ -11,9 +11,27 @@ describe RockPaperScissors::App do
       response = server.get('/')
       response.status.should == 200
     end
+  end
+
+  context '/' do
     it "Debería mostrar Bienvenido a Piedra Papel y Tijera" do
       response = server.get('/')
       response.body == 'Bienvenido a Piedra Papel y Tijera'
     end
   end
+
+  context "/?choice='Papel'" do
+    it "Debería retornar el código 200" do
+      response = server.get("/?choice='Papel'")
+      response.status.should == 200
+    end
+  end
+
+  context "/?choice='Tijera'" do
+    it "Debería retornar el código 200" do
+      response = server.get("/?choice='Tijera'")
+      response.status.should == 200
+    end
+  end
+
 end
