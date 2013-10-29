d74019247389cfca2436e7326f08e4e97eb0ca10
Diego Williams Aguilar Montaño <alu0100592368@ull.edu.es>
2013-10-29 17:42:44 +0000

    indentando correctamente

--- lib/rsack/server.rb
+++ lib/rsack/server.rb
@@ -1,16 +1,11 @@
 module Rsack
   class Server
-    
     def initialize(app=nil)
       @app = app
     end
-    
+
     def call(env)
       @app.call(env) if @app
-      #["200", {}, "hello"]
-      # response = Rack::Response.new
-      # response.write("¡Hola Mundo!")
-      # response.finish
     end
   end
 end
