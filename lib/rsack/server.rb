f6c42dcc494ee7d389d9d5fa394a60badd9c0f29
Diego Williams Aguilar Montaño <alu0100592368@ull.edu.es>
2013-10-29 17:42:01 +0000

    quitando codigo sobrante

--- lib/rsack/server.rb
+++ lib/rsack/server.rb
@@ -1,10 +1,16 @@
 module Rsack
   class Server
+    
+    def initialize(app=nil)
+      @app = app
+    end
+    
     def call(env)
+      @app.call(env) if @app
       #["200", {}, "hello"]
-      response = Rack::Response.new
-      response.write("¡Hola Mundo!")
-      response.finish
+      # response = Rack::Response.new
+      # response.write("¡Hola Mundo!")
+      # response.finish
     end
   end
 end
